e21b172174ac6a18b4d5dfb3373f1195523ac657
Miguel Mendes <mendesmiguel2007@gmail.com>
2026-03-27 20:36:05 -0300

    FrutasPreferidas

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("[Minhas Frutas Favoritas]");
+
+Console.WriteLine("1. Banana");
+Console.WriteLine("2. Pera");
+Console.WriteLine("3. Maçã");
+Console.WriteLine("4. Uva");
+Console.WriteLine("5. Pêssego");
